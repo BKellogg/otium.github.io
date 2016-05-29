6f2723e92ca38c98da263912ecc3481d1bb13755
Ryan Coffman <ryan.c432@gmail.com>
2016-05-29 00:41:24 -0700

    basic setup

---
+++ src/js/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import _ from 'bootstrap-loader';
+
+function main() {
+    ReactDOM.render(
+        <h1>Hi</h1>, document.getElementById('app-container'));
+}
+
+main();
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: path.join(__dirname, 'src/js/main.jsx'),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].js',
+        publicPath: '/assets/'
+    },
+    devtool: 'cheap-eval-source-map',
+    module: {
+        loaders: [{
+            test: /\.jsx$/,
+            loader: 'babel',
+            query: {
+                presets: ['react']
+            },
+            include: path.join(__dirname, 'src')
+        }, {
+            test: /\.scss$/,
+            loader: 'style/useable!css?root=' + path.join(__dirname, 'src') + '!sass'
+        }, {
+            test: /\.(svg|woff2?|ttf|eot)$/,
+            loader: 'url',
+            query: {
+                limit: 10000
+            }
+        }, {
+            test: /\.md$/,
+            loaders: ['html?root=' + path.join(__dirname, 'src'), 'markdown-it'],
+        }, {
+            test: /\.json$/,
+            loader: 'json'
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: "file-loader"
+        }, {
+            test: /bootstrap-sass\/assets\/javascripts\//,
+            loader: 'imports?jQuery=jquery'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        modules: [
+            path.join(__dirname, 'src'),
+            path.join(__dirname, 'src/js'),
+            path.resolve('node_modules')
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            React: 'react',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/html/index.html',
+            inject: 'body',
+            filename: 'index.html'
+        })
+    ],
+    'markdown-it': {
+        preset: 'default',
+        html: true,
+    }
+}
+
+module.exports = config;
